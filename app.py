3ff54dc84b6358de27d6a27412fac42debcb4f04
LuNK1108 <lucs.os1108@gmail.com>
2026-04-23 23:06:14 -0300

    adicionei a rota de excluir livros

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from backend.database import mostrar_livros
+from flask import Flask, render_template, jsonify, request, redirect
+from backend.database import mostrar_livros, inserir_livro, excluir_livro
 
 app = Flask(
     __name__,
@@ -15,5 +15,30 @@ def home():
 def mostra_livro():
     livros = mostrar_livros()
     return jsonify(livros)
+
+@app.route("/cadastrar_livro", methods=["POST"])
+def cadastrar_livro():
+    id_autor = int(request.form["id_autor"])
+    id_categoria = int(request.form["id_categoria"])
+    titulo = request.form["titulo"]
+    descricao = request.form["descricao"]
+    preco = float(request.form["preco"])
+    arquivo = request.form["arquivo"]
+    capa = request.form["capa"]
+    paginas = int(request.form["paginas"])
+    ativo = request.form["ativo"] == "true"
+    data = request.form["data"]
+
+    inserir_livro(id_autor,id_categoria,titulo,descricao,preco,arquivo,capa,paginas,ativo,data
+    )
+
+    return redirect("/")
+
+@app.route("/excluir_livro", methods=["POST"])
+def excluir_livros():
+    data = request.get_json()
+    id_livro = data["id"]
+    excluir_livro(id_livro)
+    return redirect("/")
 
 app.run(debug=True)
