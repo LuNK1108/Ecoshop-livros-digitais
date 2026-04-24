8c10f18a5dbbd847d1939e8eaacfc82ba02f6f0c
LuNK1108 <lucs.os1108@gmail.com>
2026-04-24 09:43:14 -0300

    adicionei os funções de validar_login e cadastr_usuario

--- backend/database.py
+++ backend/database.py
@@ -34,9 +34,41 @@ def inserir_livro(id_autor, id_categoria, titulo, descricao, preco, arquivo, cap
     conexao.commit()
     cursor.close()
 
+#excluir um livro do banco
 def excluir_livro(id_livro):
     cursor = conexao.cursor(dictionary=True)
     sql = "DELETE FROM livro WHERE id_livro = %s"
     cursor.execute(sql, (id_livro,))
     conexao.commit()
     cursor.close()
+
+#validar um login
+def validar_login(email, senha):
+    cursor = conexao.cursor(dictionary=True)
+    sql = "SELECT * FROM usuario WHERE email = %s AND senha = %s"
+    cursor.execute(sql,(email, senha))
+    usuario = cursor.fetchone()
+    cursor.close()
+
+    if usuario:
+        return True
+    else:
+        return False
+
+#cadastra usuarios
+def cadastro_usuario(nome, email, senha, telefone, tipo, data):
+    cursor = conexao.cursor(dictionary=True)
+    sql = """
+    INSERT INTO usuario 
+    (nome, email, senha, telefone, tipo_usuario, data_cadastro)
+    VALUES (%s, %s, %s, %s, %s, %s)
+    """
+    valores = (nome, email, senha, telefone, tipo, data)
+    usuario = cursor.execute(sql, valores)
+    conexao.commit()
+    cursor.close()
+
+    if usuario:
+        return True
+    else:
+        return False
